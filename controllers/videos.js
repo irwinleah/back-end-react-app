e14a3e03cab049a666bacbb014dfd4e3543459e2
Leah Irwin <irwin.leah@outlook.com>
2025-03-31 11:39:13 -0400

    testing create route with Postman

--- controllers/videos.js
+++ controllers/videos.js
@@ -4,11 +4,18 @@ const router = express.Router();
 
 // write your routes/controller functions here
 
-router.post('/', async (req, res) => {
-    // Add a message to test the route
-    res.json({ message: 'Create Route' });
-  });
-  
+// router.post('/', async (req, res) => {
+//     // Add a message to test the route
+//     res.json({ message: 'Create Route' });
+//   });
+  router.post('/', async (req, res) => {
+    try {
+        const createdVideo = await Video.create(req.body);
+        res.status(201).json(createdVideo);
+    } catch (err) {
+        res.status(500).json({ err: err.message });
+    }
+});
 
 
 
